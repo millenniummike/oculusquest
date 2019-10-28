f16dee586c01403ac32df2c3a1aaddeb1f5921d8
x <x@why.com>
2019-10-28 07:47:33 +0000

    Init

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    private AssetBundleCreateRequest bundleRequest;
+    private UnityWebRequest request;
+
+    public string url;
+    public string scene;
+
+    private void Start()
+    {
+        request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        request.Send();
+    }
+
+    private void Update()
+    {
+        if (request.isDone)
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+            Debug.Log("GOT HERE!");
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
